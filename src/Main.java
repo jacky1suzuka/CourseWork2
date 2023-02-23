96baa0e9da340db3f09c771158d70cff9b158d52
jacky <jacky1suzuka@gmail.com>
2023-02-23 16:30:08 +0300

    check

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String ss = s.nextLine();
-        System.out.println(ss);
-        String s1 = s.nextLine();
-        System.out.println(s1);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите номер команды, чтобы выбрать действие\n - - - - - - \n1.Создать задачу\n2.Завершить работу\n");
+        while (true) {
+            String command = scanner.next();
+            if (command.equals("1")) {
+                System.out.println("Введите название задачи");
+                String title = scanner.next();
+                System.out.println("Введите описание");
+                String description = scanner.next();
+                Task task = new Task(title, description);
+                System.out.println("Ваша задача:\n" + task);
+                break;
+            } else if (command.equals("2")) {
+                System.out.println("Работа завершена.");
+                break;
+            } else {
+                System.out.println("Введите корректный номер команды");
+            }
+        }
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+
+public class Task {
+    private static int idGenerator;
+    private String title;
+    private int id;
+    private String description;
+
+    public Task(String title, String description) {
+        idGenerator++;
+        this.title = title;
+        this.id = idGenerator;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && title.equals(task.title) && description.equals(task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, id, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Название: " + title + "\nОписание: " + description + "\nid: " + id ;
+    }
+}
